aa9a077e95f9f91d9bfa1bf3d8c0c4336deb7693
Jakub Meyza <kubameyza@gmail.com>
2023-08-24 22:29:02 +0200

    Remove unnecessary code

--- disney-characters-showcase/src/components/Footer/Footer.jsx
+++ disney-characters-showcase/src/components/Footer/Footer.jsx
@@ -3,7 +3,10 @@ import styles from './Footer.module.css';
 const Footer = () => {
   return (
     <div className={styles.footer}>
-      Coded by <a href="">Jakub Meyza</a>
+      Coded by{' '}
+      <a href="https://github.com/kubameyza/disney-characters-showcase/tree/main/disney-characters-showcase">
+        Jakub Meyza
+      </a>
     </div>
   );
 };
